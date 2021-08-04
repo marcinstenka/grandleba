14c2aea39881b4256ae1016d5f3655c1a5fe6f6e
Marcin Stenka <mastenka@wp.pl>
2021-08-04 17:24:45 +0200

    0.9.3 - cookies

--- src/Pages/Hotel.js
+++ src/Pages/Hotel.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import Navbar from '../Components/Navbar/Navbar';
 import Hero from '../Components/Hero/Hero';
 import Footer from '../Components/Footer/Footer';
@@ -5,15 +7,33 @@ import Footer from '../Components/Footer/Footer';
 import useMobile from '../CustomHooks/useMobile';
 
 const Hotel = () => {
-  const isMobile = useMobile();
+	const isMobile = useMobile();
+	const [cookies, setCookie] = useCookies(['cookies']);
+	const handleCookie = () => {
+		setCookie('Cookies', 'CookiesAccepted', { path: '/' });
+	};
 
-  return (
-    <>
-      <Navbar isMobile={isMobile} />
-      <Hero isMobile={isMobile} />
-      <Footer />
-    </>
-  );
+	useEffect(() => {
+		if (!cookies.Cookies) {
+			const cookiesBtn = document.querySelector('.cookies button');
+			cookiesBtn.addEventListener('click', () => {
+				cookiesBtn.parentElement.style.display = 'none';
+			});
+		}
+	}, []);
+	return (
+		<>
+			<Navbar isMobile={isMobile} />
+			{!cookies.Cookies && (
+				<div className='cookies'>
+					<p>Ta strona wykorzystuje pliki cookies.</p>{' '}
+					<button onClick={handleCookie}>X</button>
+				</div>
+			)}
+			<Hero isMobile={isMobile} />
+			<Footer />
+		</>
+	);
 };
 
 export default Hotel;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CookiesProvider } from 'react-cookie';
 import './index.css';
 import App from './App';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<CookiesProvider>
+			<App />
+		</CookiesProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
